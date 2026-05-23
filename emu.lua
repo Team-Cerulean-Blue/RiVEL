3b202c32b8710cde98da0eafc73b0d9997671dc5
WahPlus <wahplus@proton.me>
2026-05-23 20:23:16 +0300

    Added register setting and getting logic

---
+++ emu.lua
@@ -0,0 +1,28 @@
+local u32limit = 4294967296 -- Intentionally lacks a -1
+
+local regData = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0} -- Data for register 0 is intentionally not in here because it is always 0
+local registers = {}
+setmetatable(registers, {["__index"] = function(_, key)
+  local reg = {}
+  function reg.set(value)
+    regData[key] = value % u32limit
+  end
+
+  function reg.get()
+    if key == 0 then
+      return 0
+    else
+      return regData[key]
+    end
+  end
+  return reg
+end})
+
+registers[10].set(20)
+print(registers[10].get())
+registers[0].set(20)
+print(registers[0].get())
+registers[31].set(4294967297)
+print(registers[31].get())
+registers[30].set(-4294967295)
+print(registers[30].get())
